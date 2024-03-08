4292e2712788ce6a1756e7e11a28d6b6fd2a7e59
zmountains <1037233043@qq.com>
2024-03-08 10:55:35 +0800

    集成持久层Mybatis以及Mybatis官方代码生成器，访问数据库成功

---
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.jiawa.train.member.service;
+
+import com.jiawa.train.member.mapper.MemberMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    @Resource
+    private MemberMapper memberMapper;
+    public int count(){
+         return (int) memberMapper.countByExample(null);
+    }
+}

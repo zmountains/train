9affaf89f4e1c28c8da316366fd47488e53fc9c0
zmountains <1037233043@qq.com>
2024-03-10 19:19:13 +0800

    增加axios拦截器，打印请求参数和返回结果

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import DatePicker from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
 
 const app = createApp(App);
 app.use(DatePicker).use(store).use(router).mount('#app');
@@ -15,3 +16,20 @@ const icons = Icons;
 for (const i in icons) {
     app.component(i,icons[i]);
 }
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});

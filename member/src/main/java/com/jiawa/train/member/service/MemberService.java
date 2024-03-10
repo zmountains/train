3ebae9697cb5d3d22ab75f92aa04343e83744224
zmountains <1037233043@qq.com>
2024-03-10 11:05:01 +0800

    使用雪花算法生成数据库ID

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.jiawa.train.member.service;
 
 import com.jiawa.train.common.exception.BussinessException;
 import com.jiawa.train.common.exception.BussinessExceptionEnum;
+import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
@@ -30,7 +31,7 @@ public class MemberService {
 
         Member member = new Member();
         member.setMobile(mobile);
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         memberMapper.insert(member);
         return member.getId();
     }

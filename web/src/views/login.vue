c47d28040b75248790018701b4f4d52ce4e0049c
zmountains <1037233043@qq.com>
2024-03-10 19:56:47 +0800

    登录成功后，跳转到控台主页

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -41,11 +41,13 @@
 import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
 import { notification } from 'ant-design-vue';
+import { useRouter } from "vue-router";
 
 
 export default defineComponent({
   name: "login-view",
   setup() {
+    const Router = useRouter();
     const loginForm = reactive({
       mobile: '13000000000',
       code: '',
@@ -70,6 +72,8 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           notification.success({ description: '登录成功！' });
+          //登录成功，跳到控制台主页
+          Router.push("/");
         } else {
           notification.error({ description: data.message });
         }

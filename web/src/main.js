a9509fd517c86725b8227407ecf322cda9dc10dc
zmountains <1037233043@qq.com>
2024-03-10 15:01:52 +0800

    增加loin及路由界面

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { DatePicker } from 'ant-design-vue';
+import DatePicker from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 

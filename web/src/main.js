921583537e9ed58cccd1b6e881dd80c280db521c
zmountains <1037233043@qq.com>
2024-03-10 12:19:33 +0800

    集成Ant Design Vue及其图标

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(DatePicker).use(store).use(router).mount('#app');
+
+
+//全局使用图标
+const icons = Icons;
+for (const i in icons) {
+    app.component(i,icons[i]);
+}

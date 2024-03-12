13272eb59091dd4db3f413a603142dd3a1527b86
zmountains <1037233043@qq.com>
2024-03-12 19:36:35 +0800

    为axios增加登录相关拦截：请求时带上token，返回时校验返回码是不是401

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -12,16 +12,18 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
+          所有会员总数：{{count}}
         </a-layout-content>
       </a-layout>
     </a-layout>
   </a-layout>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent, ref} from 'vue';
 import TheHeader from "@/components/the-header.vue";
 import TheSider from "@/components/the-sider.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
 export default defineComponent({
   name:"main-view",
   components: {
@@ -29,7 +31,17 @@ export default defineComponent({
     TheHeader,
   },
   setup() {
+    const count = ref(0);
+    axios.get("/member/member/count").then((response) => {
+      let data = response.data;
+      if (data.success) {
+        count.value = data.content;
+      } else {
+        notification.error({ description: data.message });
+      }
+    })
     return {
+      count
     };
   },
 });

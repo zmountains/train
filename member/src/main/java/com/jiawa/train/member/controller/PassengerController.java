e02ab0471971f2b50bedbf8e90d3c1f8df725465
zmountains <1037233043@qq.com>
2024-03-14 18:30:14 +0800

    增加乘车人列表查询接口，只能查看当前会员自己添加的乘客

--- member/src/main/java/com/jiawa/train/member/controller/PassengerController.java
+++ member/src/main/java/com/jiawa/train/member/controller/PassengerController.java
@@ -1,11 +1,16 @@
 package com.jiawa.train.member.controller;
 
+import com.jiawa.train.common.context.LoginMemberContext;
 import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.member.req.PassengerQueryReq;
 import com.jiawa.train.member.req.PassengerSaveReq;
+import com.jiawa.train.member.resp.PassengerQueryResp;
 import com.jiawa.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -19,4 +24,10 @@ public class PassengerController {
         return new CommonResp<>();
     }
 
+    @GetMapping ("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
+    }
 }

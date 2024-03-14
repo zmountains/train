082dd50691003ee8dba02b872ccb7a6f9eebbca9
zmountains <1037233043@qq.com>
2024-03-14 17:37:21 +0800

    乘车人新增界面开发，增加【新增】按钮，点击新增按钮出现弹框

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>乘车人管理</h1>
+  <a-button type="primary" @click="showModal">新增</a-button>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
+           ok-text="确认" cancel-text="取消">
+    <p>Some cotents...</p>
+    <p>Some cotents...</p>
+    <p>Some cotents...</p>
+  </a-modal>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
   name:"the-welcome",
   setup() {
+    const visible = ref(false);
+
+    const showModal = () =>{
+      visible.value = true;
+    };
+
+    const handleOk = e =>{
+      console.log(e);
+      visible.value = false;
+    }
     return {
+      visible,
+      showModal,
+      handleOk
     };
   },
 });

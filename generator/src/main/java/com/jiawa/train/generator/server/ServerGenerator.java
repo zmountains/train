32f3d7eee72228c3013f7a56c181e0ec0022a39c
zmountains <1037233043@qq.com>
2024-03-17 16:16:24 +0800

    使用PassengerController制作模板，生成controller成功

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,20 +1,22 @@
 package com.jiawa.train.generator.server;
 
 import com.jiawa.train.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String servicePath = "[module]/src/main/java/com/jiawa/train/[module]/service/";
+    static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/";
     static String pomPath = "generator\\pom.xml";
     static {
-        new File(servicePath).mkdirs();
+        new File(serverPath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
@@ -25,9 +27,9 @@ public class ServerGenerator {
         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
         System.out.println("module: " + module);
 
-        servicePath = servicePath.replace("[module]", module);
+        serverPath = serverPath.replace("[module]", module);
         // new File(servicePath).mkdirs();
-        System.out.println("servicePath: " + servicePath);
+        System.out.println("serverPath: " + serverPath);
 
         // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
@@ -52,16 +54,25 @@ public class ServerGenerator {
         param.put("do_main", do_main);
         System.out.println("组装参数：" + param);
 
-        FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(servicePath+Domain+"Service.java",param);
-
+        gen(Domain, param,"service");
+        gen(Domain, param,"controller");
 //        FreemarkerUtil.initConfig("test.ftl");
 //        Map<String,Object> param = new HashMap<>();
 //        param.put("domain","Test1");
 //        FreemarkerUtil.generator(toPath+"Test1.java",param);
     }
 
-        private static String getGeneratorPath() throws DocumentException {
+    private static void gen(String Domain, Map<String, Object> param,  String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String toPath = serverPath + target + "/";
+        new File(toPath).mkdirs();
+        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
+        String fileName = toPath + Domain + Target + ".java";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");

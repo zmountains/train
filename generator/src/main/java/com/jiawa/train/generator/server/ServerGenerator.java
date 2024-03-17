84771df31237e8741bf3d098898fb00373f3d73f
zmountains <1037233043@qq.com>
2024-03-17 16:01:30 +0800

    使用PassengerSerice制作模板，生成service成功

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.jiawa.train.generator.server;
 
+import com.jiawa.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -10,14 +11,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath = "generator\\src\\main\\java\\com\\jiawa\\train\\generator\\test\\";
+    static String servicePath = "[module]/src/main/java/com/jiawa/train/[module]/service/";
     static String pomPath = "generator\\pom.xml";
     static {
-        new File(toPath).mkdirs();
+        new File(servicePath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
+        // 获取mybatis-generator
         String generatorPath = getGeneratorPath();
+
+        // 比如generator-config-member.xml，得到module = member
+        String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+
+        servicePath = servicePath.replace("[module]", module);
+        // new File(servicePath).mkdirs();
+        System.out.println("servicePath: " + servicePath);
 
         // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
@@ -26,6 +36,24 @@ public class ServerGenerator {
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+        System.out.println("组装参数：" + param);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(servicePath+Domain+"Service.java",param);
 
 //        FreemarkerUtil.initConfig("test.ftl");
 //        Map<String,Object> param = new HashMap<>();

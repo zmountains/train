cdc44784417647bf1acb608f70e8088a8e0cb0b7
zmountains <1037233043@qq.com>
2024-03-17 17:11:12 +0800

    生成saveReq实体类

--- member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
@@ -6,16 +6,44 @@ import jakarta.validation.constraints.NotBlank;
 import java.util.Date;
 
 public class PassengerSaveReq {
+
+    /**
+     * id
+     */
     private Long id;
+
+    /**
+     * 会员id
+     */
     private Long memberId;
+
+    /**
+     * 姓名
+     */
     @NotBlank(message = "【姓名】不能为空")
     private String name;
+
+    /**
+     * 身份证
+     */
     @NotBlank(message = "【身份证】不能为空")
     private String idCard;
+
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
     @NotBlank(message = "【旅客类型】不能为空")
     private String type;
+
+    /**
+     * 新增时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
+
+    /**
+     * 修改时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 

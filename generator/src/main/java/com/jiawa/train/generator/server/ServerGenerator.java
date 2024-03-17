c0cac8145ee710e5f1e8b208d79dd4da0ad7cd32
zmountains <1037233043@qq.com>
2024-03-17 17:43:35 +0800

    制作queryResp.ftl模板

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -74,9 +74,10 @@ public class ServerGenerator {
         param.put("typeSet", typeSet);
         System.out.println("组装参数：" + param);
 
-        gen(Domain, param,"service","service");
-        gen(Domain, param,"controller","controller");
-        gen(Domain, param,"req","saveReq");
+//        gen(Domain, param,"service","service");
+//        gen(Domain, param,"controller","controller");
+//        gen(Domain, param,"req","saveReq");
+        gen(Domain, param,"resp","queryResp");
 //        FreemarkerUtil.initConfig("test.ftl");
 //        Map<String,Object> param = new HashMap<>();
 //        param.put("domain","Test1");
--- member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
@@ -6,22 +6,44 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.util.Date;
 
-public class  PassengerQueryResp {
+public class PassengerQueryResp {
+
+    /**
+     * id
+     */
     @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
+    /**
+     * 会员id
+     */
     @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
+    /**
+     * 姓名
+     */
     private String name;
 
+    /**
+     * 身份证
+     */
     private String idCard;
 
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
     private String type;
 
+    /**
+     * 新增时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    /**
+     * 修改时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 

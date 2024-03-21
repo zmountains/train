da764bf67cd4dc51cc9705711b8ce40e1e5316a9
zmountains <1037233043@qq.com>
2024-03-21 19:28:48 +0800

    禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/QuartzTestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/QuartzTestJob.java
@@ -1,14 +1,26 @@
 package com.jiawa.train.batch.job;
 
 import cn.hutool.core.date.DateTime;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+
+@DisallowConcurrentExecution
 public class QuartzTestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        DateTime now = DateTime.now();
-        System.out.println("Quartz跑批任务："+now.toString());
+        DateTime begin = DateTime.now();
+        System.out.println("Quartz跑批任务开始："+begin.toString());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        DateTime end = DateTime.now();
+        System.out.println("Quartz跑批任务结束："+end.toString());
     }
 }

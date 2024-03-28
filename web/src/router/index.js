694306d682a8b443c1fb038f861db0d811e42477
zmountains <1037233043@qq.com>
2024-03-28 16:24:32 +0800

    会员端增加余票查询页面，功能和控台端完全一致

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -22,6 +22,10 @@ const routes = [
         path: 'passenger',
         component: () => import('../views/main/passenger.vue')
       },
+      {
+        path: 'ticket',
+        component: () => import('../views/main/ticket.vue')
+      },
     ]
   },
   {

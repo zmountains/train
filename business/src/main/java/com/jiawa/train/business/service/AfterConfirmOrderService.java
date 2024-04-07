eae2849741ab59ada8a46159302eb78f58dbff78
zmountains <1037233043@qq.com>
2024-04-07 21:09:40 +0800

    seata配置Nacos

--- business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
@@ -125,6 +125,7 @@ public class AfterConfirmOrderService {
             confirmOrderMapper.updateByPrimaryKeySelective(confirmOrderForUpdate);
 
             //模拟调用方出现异常
+            Thread.sleep(10000);
             if (1 == 1) {
                 throw new Exception("测试异常");
             }

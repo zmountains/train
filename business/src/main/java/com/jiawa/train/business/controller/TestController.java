4adefea33ccdd69775e2bdd52ca67f8a359de92c
zmountains <1037233043@qq.com>
2024-04-09 21:29:03 +0800

    sentinel不同流控模式讲解：链路

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,23 +1,27 @@
 package com.jiawa.train.business.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.jiawa.train.business.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    TestService testService;
 
     @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() throws InterruptedException {
-        Thread.sleep(500);
+        testService.hello2();
         return "hello world! business!";
     }
 
     @SentinelResource("hello1")
     @GetMapping("/hello1")
     public String hello1() throws InterruptedException {
-        Thread.sleep(500);
+        testService.hello2();
         return "hello world! business1!";
     }
 }

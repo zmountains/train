dc78a07bae7120166b6ba01a22a4c1f9f8adee74
zmountains <1037233043@qq.com>
2024-04-12 20:48:04 +0800

    集成RocketMQ,增加下单购票接口，只处理验证码、令牌、车次锁，不执行选座购票逻辑

--- business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.jiawa.train.business.req.ConfirmOrderDoReq;
+import com.jiawa.train.business.service.BeforeConfirmOrderService;
 import com.jiawa.train.business.service.ConfirmOrderService;
 import com.jiawa.train.common.exception.BussinessExceptionEnum;
 import com.jiawa.train.common.resp.CommonResp;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/confirm-order")
 public class ConfirmOrderController {
     @Resource
-    private ConfirmOrderService confirmOrderService;
+    private BeforeConfirmOrderService beforeConfirmOrderService;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
@@ -48,7 +49,7 @@ public class ConfirmOrderController {
             stringRedisTemplate.delete(imageCodeToken);
         }
 
-        confirmOrderService.doConfirm(req);
+        beforeConfirmOrderService.beforeDoConfirm(req);
         return new CommonResp<>();
     }
 
---
+++ business/src/main/java/com/jiawa/train/business/service/BeforeConfirmOrderService.java
@@ -0,0 +1,98 @@
+package com.jiawa.train.business.service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.jiawa.train.business.enums.RedisKeyPreEnum;
+import com.jiawa.train.business.req.ConfirmOrderDoReq;
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.exception.BussinessException;
+import com.jiawa.train.common.exception.BussinessExceptionEnum;
+import jakarta.annotation.Resource;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class BeforeConfirmOrderService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BeforeConfirmOrderService.class);
+
+    @Resource
+    public RocketMQTemplate rocketMQTemplate;
+
+    @Resource
+    private SkTokenService skTokenService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+
+    //@SentinelResource("doConfirm")
+    @SentinelResource(value = "beforeDoConfirm", blockHandler = "beforeDoConfirmBlock")
+    public void beforeDoConfirm(ConfirmOrderDoReq req){
+        // 校验令牌余量
+        boolean validSkToken = skTokenService.validSkToken(req.getDate(), req.getTrainCode(), LoginMemberContext.getId());
+        if (validSkToken) {
+            LOG.info("令牌校验通过");
+        } else {
+            LOG.info("令牌校验不通过");
+            throw new BussinessException(BussinessExceptionEnum.CONFIRM_ORDER_SK_TOKEN_FAIL);
+        }
+
+        String key = RedisKeyPreEnum.CONFIRM_ORDER + "-" + req.getDate() + "-" + req.getTrainCode();
+        // Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(key, key, 5, TimeUnit.SECONDS);
+        // if(lock){
+        //     LOG.info("恭喜，抢到锁了！");
+        // } else {
+        //     LOG.info("很遗憾，没抢到锁");
+        //     throw new BussinessException(BussinessExceptionEnum.CONFIRM_ORDER_LOCK_FAIL);
+        // }
+        RLock lock = null;
+        try {
+            //使用redisson，自带看门狗
+            lock = redissonClient.getLock(key);
+           /* waitTime ——  the maximum time to acquire the lock 等待获取锁时间（最大尝试获得锁时间），超时返回false
+            leaseTime ——  lease time 锁时长，即n秒后自动释放锁
+            time unit ——  time unit 时间单位   */
+            // boolean tryLock = lock.tryLock(0, 10, TimeUnit.SECONDS); //不带看门狗
+            boolean tryLock = lock.tryLock(0, TimeUnit.SECONDS); //带看门狗
+            if(tryLock){
+                LOG.info("恭喜，抢到锁了！");
+//                for (int i = 0; i < 30; i++) {
+//                    Long expire = stringRedisTemplate.opsForValue().getOperations().getExpire(key);
+//                    LOG.info("锁过期时间还有：{}", expire);
+//                    Thread.sleep(1000);
+//                }
+            } else {
+                //只是没抢到锁，并不知道票抢完了没，所以提示稍后再试
+                LOG.info("很遗憾，没抢到锁");
+                throw new BussinessException(BussinessExceptionEnum.CONFIRM_ORDER_LOCK_FAIL);
+            }
+
+            // 可以购票：TODO:发送MQ，等待出票
+            LOG.info("准备发送MQ，等待出票");
+        } catch (InterruptedException e) {
+            LOG.error("购票异常",e);
+        }
+    }
+
+    /**
+     * 降级方法，需包含限流方法的所有参数和BlockException参数
+     * @param req
+     * @param e
+     */
+    private void beforeDoConfirmBlock(ConfirmOrderDoReq req, BlockException e){
+        LOG.info("购票请求被限流：{}", req);
+        throw new BussinessException(BussinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+    }
+}
